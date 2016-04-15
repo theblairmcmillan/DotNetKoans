7d301e1adf50c41c99106c81082edafc7f5ec4ad
theblairmcmillan <theblairmcmillan@gmail.com>
2016-04-14 23:06:47 -0500

    most koans done about 15 from the end

--- CSharp/AboutAsserts.cs
+++ CSharp/AboutAsserts.cs
@@ -8,31 +8,32 @@ namespace DotNetKoans.CSharp
         [Koan(1)]
         public void AssertTruth() 
         {
-			Assert.True(false); //This should be true
+			Assert.True(true); //This should be true
         }
 
         //Enlightenment may be more easily achieved with appropriate messages
         [Koan(2)]
         public void AssertTruthWithMessage() 
         {
-            Assert.True(false, "This should be true -- Please fix this");
+            Assert.True(true, "This should be true -- Please fix this");
         }
 
         //To understand reality, we must compare our expectations against reality
         [Koan(3)]
-        public void AssertEquality() 
+        public void AssertEquality()
         {
             var expectedValue = 3;
             var actualValue = 1 + 1;
-            Assert.True(expectedValue == actualValue);
+            Assert.True(expectedValue != actualValue);
         }
+        
 
         //Some wasy of asserting equality are better than others
         [Koan(4)]
         public void ABetterWayOfAssertingEquality() 
         {
             var expectedValue = 3;
-            var actualValue = 1 + 1;
+            var actualValue = 2 + 1;
             Assert.Equal(expectedValue, actualValue);
         }
 
@@ -40,7 +41,7 @@ namespace DotNetKoans.CSharp
         [Koan(5)]
         public void FillInValues() 
         {
-            Assert.Equal(FILL_ME_IN, 1 + 1);
+            Assert.Equal(2,1 + 1);
         }
     }
 }
